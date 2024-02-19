f473252b70ec864e460ba9ef7bc4a09ba7bb1373
mohamedafrash <afrazzz51@gmail.com>
2024-02-19 18:57:42 +0000

    Core Java

---
+++ ForLoop/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // for loop = executes a block of code a limited amount of times
+
+        for(int i=10; i>=0; i--) {
+            System.out.println(i);
+        }
+        System.out.println("Happy new year!");
+
+        for(int x=1; x<=14; x++){
+            System.out.println(x);
+        }
+
+    }
+}
