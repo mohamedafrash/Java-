0450106c6a2cfb119bf905abfdfb65e2660c0bb8
mohamedafrash <afrazzz51@gmail.com>
2024-02-29 23:05:15 +0000

    Minor Change

--- Abstraction/src/Car.java
+++ Abstraction/src/Car.java
@@ -3,5 +3,6 @@ public class Car extends Vehicle{
     @Override
     void go() {
         System.out.println("The driver is driving the car");
+        System.out.println("Where to");
     }
 }
--- Abstraction/src/Main.java
+++ Abstraction/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         // abstract =  	abstract classes cannot be instantiated, but they can have a subclass
         //				abstract methods are declared without an implementation
 
-
+        
         Car car = new Car();
         car.go();
     }
