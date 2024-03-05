9c92f072c1d4158a20bffa65d9034ff601db4f5a
mohamedafrash <afrazzz51@gmail.com>
2024-03-05 23:05:36 +0000

    Advanced-Part4

---
+++ CopyObjects/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Vehicle car1 = new Vehicle("Chevrolet","Camaro",2021);
+        //Car car2 = new Car("Ford","Mustang",2022);
+        //car2.copy(car1);
+        Vehicle car2 = new Vehicle(car1);
+
+        System.out.println(car1);
+        System.out.println(car2);
+        System.out.println();
+        System.out.println(car1.getMake());
+        System.out.println(car1.getModel());
+        System.out.println(car1.getYear());
+        System.out.println();
+        System.out.println(car2.getMake());
+        System.out.println(car2.getModel());
+        System.out.println(car2.getYear());
+    }
+}
