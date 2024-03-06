310be99319c0d5bc08253fcf80ea3a46b9fb1b19
mohamedafrash <afrazzz51@gmail.com>
2024-03-06 23:39:39 +0000

    ..

--- Abstraction/src/Car.java
+++ Abstraction/src/Car.java
@@ -3,7 +3,7 @@ public class Car extends Vehicle{
     @Override
     void go() {
         System.out.println("The driver is driving the car");
-        System.out.println("Where to");
+        System.out.println("Where to drive");
     }
 
     @Override
